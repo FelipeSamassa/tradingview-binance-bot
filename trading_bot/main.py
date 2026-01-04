cafbeca0a89506c6bcc58e3c166be5cf2d673374
FelipeSamassa <137512643+FelipeSamassa@users.noreply.github.com>
2026-01-04 17:49:08 -0300

    tualização

--- trading_bot/main.py
+++ trading_bot/main.py
@@ -1,13 +1,11 @@
 from flask import Flask, request
 from binance.client import Client
-import os
 
+API_KEY = "SjI1fY8OJizUfo1f74KSwR1Zjm9fKHTlEPCgMvd5huKBgMwPyccUxZwKRvZm2UMA"
+API_SECRET = "SfrxIXHGXc609ZQh4lLtdHG2LnMdh9fVP4p87D9gv9XbnFnh4IR8BoTxpLmqqVNr"
+
+client = Client(API_KEY, API_SECRET)
 app = Flask(__name__)
-
-api_key = os.getenv("BINANCE_API_KEY")
-api_secret = os.getenv("BINANCE_SECRET_KEY")
-
-client = Client(api_key, api_secret)
 
 @app.route("/", methods=["GET"])
 def home():
@@ -26,4 +24,7 @@ def webhook():
         type="MARKET",
         quantity=quantity
     )
+
     return {"status": "ok", "order": order}
+
+app.run(port=5000)
